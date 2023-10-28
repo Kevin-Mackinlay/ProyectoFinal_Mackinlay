8c18a6a319bccddde374f158aefd237c6235c4e3
Mackinlay Kevin <kemack83@gmail.com>
2023-10-28 19:42:06 -0300

    ItemDetail contador

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ producto }) => {
+    const [cantidadAgregada, setCantidadAgregada] = useState('')
   const onAdd = (cantidad) => {
     console.log(`Compraste ${cantidad} productos`);
+    setCantidadAgregada(cantidad)
   };
+
+  console.log(cantidadAgregada);
 
   return (
     <div className="d-flex flex-column align-items-center">
@@ -12,7 +17,8 @@ const ItemDetail = ({ producto }) => {
       <img src={producto.img} alt={producto.name}></img>
       <p>{producto.description}</p>
       <p>${producto.price}</p>
-      <ItemCount initial={1} stock={producto.stock} onAdd={onAdd} />
+     { cantidadAgregada === '' ? <ItemCount initial={1} stock={producto.stock} onAdd={onAdd} />
+   : <Link to='/cart' className='btn btn-secondary'>Ir al carrito</Link>}
     </div>
   );
 };
